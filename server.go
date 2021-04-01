952fd27f763c97dd03a5b90750c0069a6b9250f7
David Yappeter <davidyap11les@gmail.com>
2021-04-01 23:45:18 +0700

    Initialize Simple Rest

---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import(
+  "github.com/gorilla/mux"
+  "log"
+  "os"
+  "fmt"
+  "net/http"
+)
+
+var temp = 0
+
+func main() {
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+  }
+
+  router := mux.NewRouter()
+  router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Hello World!")
+  })
+
+  router.HandleFunc("/hit", func(w http.ResponseWriter, r *http.Request) {
+    temp++
+    fmt.Fprintf(w, "I have been hit %d time(s)", temp)
+  })
+
+  log.Println("Listen and Serve at http://localhost:"+port)
+  log.Println(http.ListenAndServe(":"+port, router))
+}
+
